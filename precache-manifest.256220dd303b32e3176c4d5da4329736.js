88368fbf26ea752d776473ecdffc6dab94b603c7
Kirill Medved <medved.kirill@bk.ru>
2020-12-31 02:19:20 +0300

    Updates

---
+++ precache-manifest.256220dd303b32e3176c4d5da4329736.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2d9814420286b52e36e9ac71ca5d12",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "dc1e1ebb2282eb517da2",
+    "url": "/social-network-react/static/css/main.816f36db.chunk.css"
+  },
+  {
+    "revision": "0ec714170eb054b991b9",
+    "url": "/social-network-react/static/js/2.1d3cebed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-react/static/js/2.1d3cebed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1e1ebb2282eb517da2",
+    "url": "/social-network-react/static/js/main.4836d9a5.chunk.js"
+  },
+  {
+    "revision": "aefd7756755ec693825f",
+    "url": "/social-network-react/static/js/runtime-main.1aaad446.js"
+  },
+  {
+    "revision": "c84c40959a967e0d4f7aeeb3353c9723",
+    "url": "/social-network-react/static/media/Preloader.c84c4095.png"
+  },
+  {
+    "revision": "800ad3c90da7fe1385ffcabc0ccce4c6",
+    "url": "/social-network-react/static/media/userImage.800ad3c9.jpg"
+  }
+]);
